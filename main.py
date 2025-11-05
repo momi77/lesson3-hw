db24898a50f1f1cf333cdc75579d9789c7075196
momi77 <anotp96@gmail.com>
2025-11-05 20:32:05 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import flet as ft
+
+
+def main_page(page: ft.Page):
+    page.title = 'My first app'
+    greeting_text = ft.Text(value='Hello world')
+
+
+    def on_button_click(_):
+        name = name_input.value.strip()
+        #print(name)
+        
+        if name:
+            greeting_text.color = None
+            greeting_text.value = f'Hello {name}'
+            name_input.value = None
+            
+        else:
+            greeting_text.value = "You didn't enter the name!!!"
+            greeting_text.color = ft.Colors.RED
+
+        page.update()
+
+    name_input = ft.TextField(label='Enter the name', on_submit=on_button_click)
+    input_button_text = ft.TextButton(text = 'send', icon=ft.Icons.SEND_ROUNDED, on_click= on_button_click)
+
+    
+    page.add(greeting_text, name_input, input_button_text)
+
+ft.app(target=main_page)
+
+
+
+
+
+
+
+
